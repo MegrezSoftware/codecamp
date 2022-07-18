a6936922f10473ad8e8698fc1cd4d717c9b8ea5d
muchuanxin <muchuanxin@fenbi.com>
2022-07-18 21:12:31 +0800

    ADD: practice 2

--- Mahjong.kt
+++ Mahjong.kt
@@ -6,8 +6,11 @@ fun main() {
     require(convertInputAndOutput("1112345678999") == "123456789")
     require(convertInputAndOutput("23456-22233-789") == "147")
     require(convertInputAndOutput("123456-23444-55") == "-14-5")
-    require(convertInputAndOutput("1112378999-123-") == "1469")
+    require(convertInputAndOutput("1112378999-123--") == "1469")
     require(convertInputAndOutput("2344445688999") == "178")
+    require(convertInputAndOutput("223344-234-223-1") == "--12345")
+    require(convertInputAndOutput("1234489-147-1234") == "null")
+    require(convertInputAndOutput("123456789---1234") == "all")
 }
 
 fun convertInputAndOutput(input: String): String {
@@ -36,6 +39,12 @@ fun convertInputAndOutput(input: String): String {
     if (tiao.isNotEmpty()) {
         wan.append("-").append(tiao)
     }
+    if (wan.isEmpty()) {
+        wan.append("null")
+    }
+    if (wan.length == 29) {
+        wan.clear().append("all")
+    }
     return wan.toString()
 }
 
@@ -45,21 +54,33 @@ fun convertInputAndOutput(input: String): String {
 fun whichCardToTing(handCards: List<Mahjong>): List<Mahjong> {
     if (handCards.size != 13)
         return emptyList()
+    // 过滤万能牌
+    val remainCards = handCards.filter { it.isOmnipotentCard().not() }
+    // 统计万能牌个数
+    val omnipotentCount = handCards.count { it.isOmnipotentCard() }
     val counts = IntArray(31)
-    handCards.forEach {
+    remainCards.forEach {
         counts[it.value]++
     }
     val results = mutableListOf<Mahjong>()
     outer@
-    for (tingCard in Mahjong.values()) {
-        if (worthNotToTing(counts, tingCard.value))
+    for (tingCard in Mahjong.values().filter { it.isOmnipotentCard().not() }) {
+        if (worthNotToTing(counts, tingCard.value, omnipotentCount))
             continue
         val tingCounts = counts.copyOf().apply { this[tingCard.value]++ }
         for (index in tingCounts.indices) {
             // 扣除单个对子
-            if (tingCounts[index] >= 2 && judgeRemainAllKeAndShun(tingCounts.copyOf().apply { this[index] -= 2 })) {
-                results.add(tingCard)
-                continue@outer
+            var reduceCount: Int
+            if (tingCounts[index] + omnipotentCount >= 2) {
+                val cards = tingCounts.copyOf().apply {
+                    // 如果个数不够2，用万能牌凑数
+                    reduceCount = (2 - this[index]).coerceAtLeast(0)
+                    this[index] = (this[index] - 2).coerceAtLeast(0)
+                }
+                if (judgeRemainAllKeAndShun(cards, omnipotentCount - reduceCount)) {
+                    results.add(tingCard)
+                    continue@outer
+                }
             }
         }
     }
@@ -70,35 +91,40 @@ fun whichCardToTing(handCards: List<Mahjong>): List<Mahjong> {
  * 判断是否值得听的牌
  * @return true不值得听
  */
-fun worthNotToTing(counts: IntArray, tingCard: Int): Boolean {
-    return counts[tingCard] + counts[tingCard - 1] + counts[tingCard + 1] == 0 || counts[tingCard] == 4
+fun worthNotToTing(counts: IntArray, tingCard: Int, omnipotentCount: Int): Boolean {
+    return counts[tingCard] + counts[tingCard - 1] + counts[tingCard + 1] + omnipotentCount == 0 || counts[tingCard] == 4
 }
 
 /**
  * 判断剩余牌是否全是刻和顺
  */
-fun judgeRemainAllKeAndShun(counts: IntArray): Boolean {
+fun judgeRemainAllKeAndShun(counts: IntArray, omnipotentCount: Int): Boolean {
+    var remainOmnipotent = omnipotentCount
     for (index in 0..counts.lastIndex - 2) {
-        if (counts[index] == 0) continue
-        if (counts[index] in 1..2) {
-            if (counts[index + 1] < counts[index] || counts[index + 2] < counts[index])
-                return false
-            else {
-                counts[index + 1] -= counts[index]
-                counts[index + 2] -= counts[index]
-                counts[index] = 0
+        if (counts[index] <= 0) continue
+        when (counts[index]) {
+            2 -> {
+                if ((counts[index + 1] - 2).coerceAtMost(0) + (counts[index + 2] - 2).coerceAtMost(0) < -1 && remainOmnipotent >= 1) {
+                    remainOmnipotent--
+                } else if ((counts[index + 1] - 2).coerceAtMost(0) + (counts[index + 2] - 2).coerceAtMost(0) + remainOmnipotent < 0) {
+                    return false
+                } else {
+                    counts[index + 1] -= 2
+                    counts[index + 2] -= 2
+                    remainOmnipotent += ((counts[index + 1].coerceAtMost(0)) + (counts[index + 2].coerceAtMost(0)))
+                }
             }
-        } else if (counts[index] == 4) {
-            if (counts[index + 1] < 1 || counts[index + 2] < 1)
-                return false
-            else {
+            1, 4 -> {
+                if ((counts[index + 1] - 1).coerceAtMost(0) + (counts[index + 2] - 1).coerceAtMost(0) + remainOmnipotent < 0) {
+                    return false
+                }
                 counts[index + 1] -= 1
                 counts[index + 2] -= 1
-                counts[index] = 0
+                remainOmnipotent += ((counts[index + 1].coerceAtMost(0)) + (counts[index + 2].coerceAtMost(0)))
             }
         }
     }
-    return counts[29] % 3 == 0
+    return (counts[29] + remainOmnipotent) % 3 == 0
 }
 
 enum class Mahjong(val value: Int) {
@@ -109,7 +135,14 @@ enum class Mahjong(val value: Int) {
     TONG1(11), TONG2(12), TONG3(13), TONG4(14), TONG5(15), TONG6(16), TONG7(17), TONG8(18), TONG9(19),
 
     // 条1~9
-    TIAO1(21), TIAO2(22), TIAO3(23), TIAO4(24), TIAO5(25), TIAO6(26), TIAO7(27), TIAO8(28), TIAO9(29);
+    TIAO1(21), TIAO2(22), TIAO3(23), TIAO4(24), TIAO5(25), TIAO6(26), TIAO7(27), TIAO8(28), TIAO9(29),
+
+    // 万能牌，春夏秋冬梅兰竹菊
+    SPRING(31), SUMMER(32), AUTUMN(33), WINTER(34), PLUM(35), ORCHID(36), BAMBOO(37), CHRYSANTHEMUM(38);
+
+    fun isOmnipotentCard(): Boolean {
+        return this in listOf(SPRING, SUMMER, AUTUMN, WINTER, PLUM, ORCHID, BAMBOO, CHRYSANTHEMUM)
+    }
 
     companion object {
         fun parse(value: Int): Mahjong? {
