7c039cf71fe2f69670c002210aae83751aa8cc47
jiyingdabj <jiyingdabj@yuanfudao.com>
2022-07-29 17:58:26 +0800

    ADD: 注释

--- src/main/java/com/jiyingda/megrez/MahjongMain.java
+++ src/main/java/com/jiyingda/megrez/MahjongMain.java
@@ -17,16 +17,24 @@ public class MahjongMain {
     public static void main(String[] args) {
         List<Mahjong> mahjongs = Mahjong.toMjs(Arrays.asList(3,3,4,4,4,5,5,5,6,12,13,13,13));
         List<Mahjong> lessonCards = getAllListenCards(mahjongs);
+        List<Mahjong> lessonCards2 = getAllListenCardsWithHua(mahjongs);
         Mahjong.print(lessonCards);
-
+        Mahjong.print(lessonCards2);
     }
 
+    /**
+     * 听牌的判断
+     */
     public static List<Mahjong> getAllListenCards(List<Mahjong> mahjongs) {
         int[] mjs = Mahjong.toCodes(mahjongs);
         List<Integer> lessonCards = Solution1.check13(mjs);
         return Mahjong.toMjs(lessonCards);
     }
 
+    /**
+     * 带花的听牌判断
+     * 功能包含 #getAllListenCards
+     */
     public static List<Mahjong> getAllListenCardsWithHua(List<Mahjong> mahjongs) {
         int[] mjs = Mahjong.toCodes(mahjongs);
         List<Integer> lessonCards = Solution2.check13(mjs);
