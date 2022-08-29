75d5c3ae9499538324ce392e392f20bafd337cfd
tangjing01 <tangjing01@fenbi.com>
2022-08-30 00:23:31 +0800

    MOD: practice5

--- code/src/com/tangtang/practice5/BinaryNode.kt
+++ code/src/com/tangtang/practice5/BinaryNode.kt
@@ -10,11 +10,17 @@ fun main() {
                     Node(2,
                             Node(5, null, null), Node(6, null, null)))
 
-//    recursePreorder(root)// 3 1 4 0 5 2 6
-//    loopPreorder(root)// 3 1 4 0 5 2 6
+//    recursePreorder(root)// 0 1 3 4 2 5 6
+//    loopPreorder(root)// 0 1 3 4 2 5 6
 
-//    loopBfs(root)// 0 1 2 3 4 5 6
-    recurseBfs(root)// 0 1 2 3 4 5 6
+//    recurseInorder(root)// 3 1 4 0 5 2 6
+//    loopInorder(root)// 3 1 4 0 5 2 6
+
+//    recursePostorder(root)// 3 4 1 5 6 2 0
+    loopPostorder(root)// 3 4 1 5 6 2 0
+
+    loopBT(root)// 0 1 2 3 4 5 6
+    recurseBT(root)// 0 1 2 3 4 5 6
 }
 
 data class Node(
@@ -22,16 +28,15 @@ data class Node(
         val left: Node?,
         val right: Node?,
 ) {
-    // loopPreorder
-    var isAccess = false
+    // loopPreorder loopPostorder
+    var hasAccess = false
 }
-
 
 fun recursePreorder(root: Node?) {
     if (root == null) return
 
-    recursePreorder(root.left)
     println(root.value)
+    recursePreorder(root.left)
     recursePreorder(root.right)
 }
 
@@ -41,22 +46,74 @@ fun loopPreorder(root: Node?) {
     val stack = Stack<Node>()
     stack.push(root)
     while (stack.isNotEmpty()) {
+        stack.pop().run {
+            print(value)
+            right?.let {
+                stack.push(it)
+            }
+            left?.let {
+                stack.push(it)
+            }
+        }
+    }
+}
+
+fun recurseInorder(root: Node?) {
+    if (root == null) return
+
+    recurseInorder(root.left)
+    println(root.value)
+    recurseInorder(root.right)
+}
+
+fun loopInorder(root: Node?) {
+    if (root == null) return
+
+    val stack = Stack<Node>()
+    stack.push(root)
+    while (stack.isNotEmpty()) {
         val peek = stack.peek()
-        if (peek.left?.isAccess == false) {
+        if (peek.left?.hasAccess == false) {
             stack.push(peek.left)
         } else {
             println(peek.value)
-            peek.isAccess = true
+            peek.hasAccess = true
             stack.pop().right?.let {
                 stack.push(it)
             }
         }
     }
-
 }
 
-// better?
-fun recurseBfs(root: Node?) {
+fun recursePostorder(root: Node?) {
+    if (root == null) return
+
+    recursePostorder(root.left)
+    recursePostorder(root.right)
+    println(root.value)
+}
+
+fun loopPostorder(root: Node?) {
+    if (root == null) return
+
+    val stack = Stack<Node>()
+    stack.push(root)
+    while (stack.isNotEmpty()) {
+        val peek = stack.peek()
+        when {
+            peek.left?.hasAccess == false -> stack.push(peek.left)
+            peek.right?.hasAccess == false -> stack.push(peek.right)
+            else -> {
+                println(peek.value)
+                peek.hasAccess = true
+                stack.pop()
+            }
+        }
+    }
+}
+
+// todo better?
+fun recurseBT(root: Node?) {
     if (root == null) return
 
     fun innerRecurse(queue: Queue<Node>) {
@@ -77,7 +134,7 @@ fun recurseBfs(root: Node?) {
     innerRecurse(LinkedList<Node>().apply { offer(root) })
 }
 
-fun loopBfs(root: Node?) {
+fun loopBT(root: Node?) {
     if (root == null) return
 
     val queue = LinkedList<Node>()
